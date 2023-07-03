b61cf8dd140a8ff22d4f656b825f8f047ea89395
coodori <daoh98@naver.com>
2023-07-03 10:47:02 +0900

    add: Logtrace abstract template

---
+++ src/object/abstracttemplate/SubClassLogic2.java
@@ -0,0 +1,8 @@
+package object.abstracttemplate;
+
+public class SubClassLogic1 extends AbstractTemplate{
+    @Override
+    protected void call() {
+        System.out.println("SubClassLogic1.call");
+    }
+}
